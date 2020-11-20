c82f2072ea3c26ab77d80f82286f954a8d8f4831
fabienhinault <fabienhinault@users.noreply.github.com>
2020-11-20 14:39:15 +0100

    Update sqlfe.bash

--- bin/sqlfe.bash
+++ bin/sqlfe.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ ! -e $1 ]
+if [ -e $1 ]
 then
+  /home/fabien/bin/backup.sh $1
+else 
   mkdir -p $(dirname $1)
-  vim $1
 fi
-
+vim $1
 psql "${DB_STRING}" -f $1
+/home/fabien/bin/backup.sh $1
