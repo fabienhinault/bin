54ffa6d0f6d3a6f12b1eba8c8818dc2be7846316
fabienhinault <fabienhinault@users.noreply.github.com>
2023-10-31 11:18:01 +0100

    Update curl_no_data.bash

--- bin/curl_no_data.bash
+++ bin/curl_no_data.bash
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-AUTH_HOST=$1
-REALM=$2
-USER=$3
-PASS=$4
-HOST=$5
-URL_PATH=$6
-VERB=$7
+AUTH_URL=$1
+USER=$2
+HOST=$3
+URL_PATH=$4
+VERB=$5
+
+if [ -z "$PASS" ]
+then
+    read -sp "$USER's password" PASS
+    export PASS
+    echo
+fi
+
+if [ -z "$SECRET" ]
+then
+    read -sp "client secret" SECRET
+    export SECRET
+    echo
+fi
+
 
 TOKEN=$(curl -d 'client_id=client' -d "username=$USER" -d "password=$PASS" -d 'grant_type=password' "${AUTH_HOST}/realms/$REALM/protocol/openid-connect/token" | jq '.access_token' | tr -d '"')
 
