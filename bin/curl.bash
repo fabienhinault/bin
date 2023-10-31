4e4c1cde6e14c300f8e7205a13ae5fe14f5aa5a1
fabienhinault <fabienhinault@users.noreply.github.com>
2023-10-31 11:25:06 +0100

    Update curl.bash

--- bin/curl.bash
+++ bin/curl.bash
@@ -1,16 +1,50 @@
 #!/bin/bash
 
-AUTH_HOST=$1
-REALM=$2
-USER=$3
-PASS=$4
-HOST=$5
-URL_PATH=$6
-DATA_PATH=$7
-VERB=$8
-CLIENT_ID=$9
+set -euo pipefail
 
-TOKEN=$(curl -d "client_id=${CLIENT_ID}" -d "username=$USER" -d "password=$PASS" -d 'grant_type=password' "${AUTH_HOST}/realms/$REALM/protocol/openid-connect/token" | jq '.access_token' | tr -d '"')
+AUTH_URL=$1
+shift
+USER=$1
+shift
+HOST=$1
+shift
+URL_PATH=$1
+SUBSTITUTED_PATH=$URL_PATH
+shift
+DATA_PATH=$1
+shift
+VERB=$1
+shift
+
+TMPDIR=$(mktemp --directory)
+cp "${DATA_PATH}/${URL_PATH}/${VERB}.json" "$TMPDIR/${VERB}.json"
+
+iSubst=0
+while [ "$#" -gt "0" ]
+do
+    SUBSTITUTED_PATH=${SUBSTITUTED_PATH/_/$1}
+    sed --in-place=~$iSubst "s/\"{$iSubst}\"/$1/g" "$TMPDIR/${VERB}.json"
+    shift
+    ((iSubst++)) || true
+done
+
+if [ -z "${PASS+x}" ]
+then
+    read -sp "$USER's password" PASS
+    export PASS
+    echo
+fi
+
+if [ -z "${SECRET+x}" ]
+then
+    read -sp "client secret" SECRET
+    export SECRET
+    echo
+fi
+
+
+
+TOKEN=$(curl -d "client_id=${CLIENT_ID}" -d "username=$USER" -d "password=$PASS" -d 'grant_type=password' "${AUTH_URL}" | jq '.access_token' | tr -d '"')
 
 echo $TOKEN
 
